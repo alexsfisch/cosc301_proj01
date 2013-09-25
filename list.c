28507cc1b018772df04d7613b48a57115f5c0fbf
cosc301 <cosc301@csvm.(none)>
2013-09-24 22:57:41 -0400

    almost done

--- list.c
+++ list.c
@@ -11,7 +11,7 @@
 
 void list_insert(const int integer, struct node **head) {
      struct node *newnode = malloc(sizeof(struct node));
-     newnode->integer = integer; //need to malloc this
+     newnode->integer = integer; //need to malloc this so that we can free new below
      newnode->next = *head;
      *head = newnode;
 }
@@ -31,14 +31,11 @@ void buildLL(char* line, char* delim, struct node **head, const int stringLength
 	while(token!=NULL) {
 		i = atoi(token);
 		if (i!=0) { //returns 0 if not digit
-			list_insert(i,&new); //add integer token
-			list_sort(&new);
-		}
-		/*else {
-			if(isdigit(token)){
-				list_insert(token,&new);
+			if (i&1==0){
+				list_insert(i,&new); //add integer token
+				list_sort(&new);
 			}
-		}*/
+		}
 		token = strtok(NULL, delim);	//walk through tokens in current line
 	}
 
